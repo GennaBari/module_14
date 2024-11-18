07e6c590ba7fcf31e7341415b804e73fd99903ee
GennaBari <barinovaevgenia84@gmail.com>
2024-11-18 18:06:08 +0300

    Update module_14_1.py
    Создание БД, добавление, выбор и удаление элементов.

--- module_14_1.py
+++ module_14_1.py
@@ -10,7 +10,7 @@ id INTEGER PRIMARY KEY,
 username TEXT NOT NULL,
 email TEXT NOT NULL,
 age INTEGER,
-balance INTEGER TEXT NOT NULL
+balance INTEGER 
 )
 ''')
 
@@ -18,13 +18,13 @@ balance INTEGER TEXT NOT NULL
 
 for i in range(1,11):
     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
-                      (f'User{i + 1}', f'example{i}gmail.com', f'{i * 10}', 1000))
+                      (f'User{i + 1}', f'example{i}@gmail.com', f'{i * 10}', 1000))
 
 for i in range(1,11,2):
-    cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"User"))
+    cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"User{i}"))
 
 for i in range(1,11,3):
-    cursor.execute("DELETE FROM Users WHERE username = ?", (f"User",))
+    cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i}",))
 
 cursor.execute("SELECT username, email, age, balance FROM Users WHERE AGE != 60")
 
